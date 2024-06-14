46fd19e0887e69b69cfdb472ef9351abbb79c753
Nikita Orlov <stringnickq@gmail.com>
2024-06-14 14:07:44 +0200

    fix debug test for std big int is prime

--- src/math/fields/prime.zig
+++ src/math/fields/prime.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const prime = @cImport(@cInclude("prime.h"));
 
 pub fn isPrimeStdBigInt(t: std.math.big.int.Managed) bool {
-    return prime.is_prime(@ptrCast(&t.limbs[0]), t.limbs.len);
+    return prime.is_prime(@ptrCast(&t.limbs[0]), t.metadata);
 }
 
 pub fn isPrime(comptime T: type, t: T) bool {
@@ -17,9 +17,10 @@ pub fn isPrime(comptime T: type, t: T) bool {
 test "test_is_prime" {
     try std.testing.expectEqual(true, isPrime(u256, 20988936657440586486151264256610222593863921));
 }
-// test "test_prime" {
-//     var t = try std.math.big.int.Managed.initSet(std.testing.allocator, 20988936657440586486151264256610222593863921);
-//     defer t.deinit();
 
-//     try std.testing.expectEqual(true, isPrimeStdBigInt(t));
-// }
+test "test_prime" {
+    var t = try std.math.big.int.Managed.initSet(std.testing.allocator, 179000439371151441295299346079990763017);
+    defer t.deinit();
+
+    try std.testing.expectEqual(true, isPrimeStdBigInt(t));
+}
